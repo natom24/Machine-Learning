b4fecf00d0f0a1d41e8f0af783cfb7e711489286
natom24 <nhaulk1@lsu.edu>
2022-05-17 15:46:49 -0500

    Pulls photos

--- Training.py
+++ Training.py
@@ -1,4 +1,26 @@
-
 import os
 import json
+import PIL
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+
+
+
+# Loads data into 
+img = []
+
+for file in os.listdir('Data/Images'):
+    img_data = tf.io.read_file('Data/Images/%s'%(file))
+    img_data = tf.io.decode_jpeg(img_data)
+    img.append(img_data)
+
+
+labels = [{'name':'Hemo', 'id':1}, {'name':'Cell', 'id':2}] #Creates the labels for the two objects being tested
+
+# Limit GPU Consumption 
+# Doesn't work on windows
+#x = tf.config.experimental.list_physical_devices('')
 
