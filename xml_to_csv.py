c37d5f349ab7af27d083e38584dec510c5451f6e
natom24 <nhaulk1@lsu.edu>
2022-05-30 13:28:42 -0500

    fixed recording bug

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -9,10 +9,8 @@ import xml.etree.ElementTree as ET
 
 def xml_to_csv(path):
     
-    
-    
     box_list = []
-    for f in glob.glob(path+'/*.xml'):
+    for f in glob.glob(path+'\*.xml'):
         tree = ET.parse(f)
         root = tree.getroot()
         
@@ -26,11 +24,11 @@ def xml_to_csv(path):
             box_data = os.path.basename(f), width, height, x.find('name').text, box_size.find('xmin').text, box_size.find('ymin').text, box_size.find('xmax').text, box_size.find('ymax').text
             box_list.append(box_data)
 
-        return box_list
+    return box_list
 
 def main():
     path = sys.argv[1]
     data = xml_to_csv(path = path)
-    np.savetxt(os.path.basename(path)+"_data.csv", data, delimiter =", ", fmt ='% s')
+    np.savetxt(path+'_data.csv', data, delimiter =", ", fmt ='% s')
 
 main()
