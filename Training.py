7521e520997ee4fb275cf29e5f31ab7ee4742c9f
natom24 <nhaulk1@lsu.edu>
2022-06-06 08:37:15 -0500

    Added model files from zoo

--- Training.py
+++ Training.py
@@ -9,69 +9,49 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+########################### Paths ###########################
+
+
+
 ##################### Create Labelmap ######################## 
 
-label_list = ["hemo","cell"]
-path = 'Data\\'
+label_list = ["hemo","cell"] # Label list used here
+path = 'Data\\' # Default Path
 
 # Function to create pbtext file containing labels
 def create_labelmap(labels = label_list, path = path):
     
     with open(path + 'label_map.pbtxt', 'w') as f: # Creates the labelmap 
-        x = 1 # Initial ID value
+        ID = 1 # Initial ID value
         
         # Goes through each label writing the appropriate labelmap format
         for label in label_list:
             f.write ("item {\n")
-            f.write ("\tid: %s\n"%(x))
-            f.write ("\tname: '"+ label_list[x-1] + "'\n")
+            f.write ("\tid: %s\n"%(ID))
+            f.write ("\tname: '"+ label_list[ID-1] + "'\n")
             f.write ("}\n")
             
-            x = x+1 #Increments 
-            
-#create_labelmap()
+            ID = ID+1 #Increments ID value
+
 
 ################### Loads Images In ######################
 
 
 img = [] # Blank list for images
 
-for file in os.listdir('../Code/TestData/train'): # Loop to open and save each file to a list
-    if file.endswith(".jpg"):  
-        img_data = PIL.Image.open('../Code/TestData/train/%s'%(file)) # Reads in file
+for file in os.listdir('Data/Images/test'): # Loop to open and save each file to a list
+    if file.endswith(".JPG"): # Only open .jpg images 
+        img_data = PIL.Image.open('Data/Images/test/%s'%(file)) # Reads in file
         img.append(img_data) # Adds photo data to list
 
-plt.imshow(img[2]) #Plots one photo
+plt.imshow(img[1]) #Plots one photo
 
     
     
-
-
-
-########################### Split Data ##########################
-
-'''
-Come back to once tfrecords established and labels. 
-Currently splitting through conversion to csv. Best method?
-Also do I need validation?
-'''
-
-# Splits the data into the train and test sets
-#train, test = train_test_split(img, test_size = .2)
-
-
-# Adds a validation set (DO I NEED THIS!!!!!!)
-#train, val = train_test_split(train, test_size = .2)
-
-
-#plt.imshow(test[0])
-
-
-#labels = [{'name':'Hemo', 'id':1}, {'name':'Cell', 'id':2}] #Creates the labels for the two objects being tested
-
-
 ########################### Model ##########################
 
-model_path = ''
+model_path = 'Models/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8/saved_model'
 model_name = 'faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz'
 link = 'https://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz'
+
+model = tf.saved_model.load(model_path)
