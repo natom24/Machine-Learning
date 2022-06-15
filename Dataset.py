242a72b6107975cf41f9d5e90574c0c207145e11
natom24 <nhaulk1@lsu.edu>
2022-06-15 15:54:28 -0500

    finished __getitem__

--- Dataset.py
+++ Dataset.py
@@ -20,21 +20,49 @@ class HemocyteDataset(torch.utils.data.Dataset):
         """
         
         self.root = root
-        self.annot = pd.read_csv(os.path.join(root, 'Data', 'Boxes', annot), header = None)
+        self.annot = pd.read_csv(os.path.join(root, 'Data', annot))
         self.transform = transform
         
         self.img = list(os.listdir(os.path.join(root,'Data',"Images")))
         
     
     def __getitem__(self,idx):
-        
-        img_path = os.path.join(self.root,'Data',"Images",self.img[idx])
-        annot_name = re.sub('\.JPG','.xml',self.img[idx])
+        """
+        Load in annotations and images
+        """
+        # Pulls a string with the path to the selected image
+        img_path = os.path.join(self.root,'Data',"Images",self.img[idx]) 
+        annot_name = re.sub('\.JPG','.xml',self.img[idx]) # Get name of file with xml ending
         
         img = Image.open(img_path)
         img = img.convert('RGB')
         
+        label_data = self.annot[self.annot.iloc[:,0] == annot_name]
+        
+        boxes = []
+        labels = []
+        area = []
+        iscrowd = []
+        
+        label_data[:,3] = pd.factorize(label_data[:,3])
+        
+        for l in label_data:
+            
+            
+            xmin = label_data[l,4]
+            ymin = label_data[l,5]
+            xmax = label_data[l,6]
+            ymax = label_data[l,7]
+            labels.append(label_data[l,3])
+            boxes.append([[xmin, ymin, xmax, ymax]])
+            area.append((xmax-xmin)*(ymax-ymin))
+            
+
         target = {}
+        target['boxes'] = torch.as_tensor(boxes,dtype=torch.float32)
+        target['labels'] = torch.as_tensor(labels, dtype=torch.int64)
+        target['iscrowd'] = torch.zeros(boxes.shape[0], dtype=torch.int64)
+        target['image_id'] = torch.as_tensor([idx])
         
         
         return img,target
