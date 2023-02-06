520a3285c2d04f70cb18b2874dc358d1b1472012
natom24 <nhaulk1@lsu.edu>
2023-02-06 12:34:58 -0600

    Reorganized and updated photos

---
+++ count_compare.py
@@ -0,0 +1,9 @@
+import os
+
+def count_compare(origin_path):
+    
+    o_path = os.path.join(origin_path, "Photos")
+    ml_path = os.path.join(origin_path, "Yolo_Boxes")
+    
+count_compare(origin = 'C:\School\Project\Machine-Learning\Hemo_data\Small_Grid')
+
